9c387ff62c7cce525801cceb56be99fe25afc1c9
vanamalapk <123520278+vanamalapk@users.noreply.github.com>
2023-03-27 23:34:37 -0500

    updated user.js

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
+let sum =0;
+let more =0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   more = more+1;
   sum = more+sum;
-  res.send('Sum is'+sum);
+  res.send('Sum is '+sum);
 });
 
 module.exports = router;
